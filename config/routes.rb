419939d55061d7dc9220cf36fed711ac099d4c63
Aline Marques <alinem_oliveira@yahoo.com>
2025-12-28 15:16:19 -0300

    Fixed code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :shares, only: [:new, :create]
+  resources :shares, only: [ :new, :create ]
   get "/:slug" => "shares#show", as: :slug
 end
